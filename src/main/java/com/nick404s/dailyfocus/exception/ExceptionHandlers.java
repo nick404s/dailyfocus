8f30dfdf3996294307c53106b84ca72a3a942df3
nick404s <nickskryabin@gmail.com>
2026-02-12 12:17:36 -0800

    added global and custom exception handling for the api

---
+++ src/main/java/com/nick404s/dailyfocus/exception/ExceptionHandlers.java
@@ -0,0 +1,34 @@
+package com.nick404s.dailyfocus.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+
+@ControllerAdvice
+public class ExceptionHandlers {
+
+    @ExceptionHandler(ResponseStatusException.class) // custom response exception handling
+    public ResponseEntity<ExceptionResponse> handleException(ResponseStatusException responseStatusException){
+        return buildResponseEntity(
+                responseStatusException,
+                HttpStatus.valueOf(responseStatusException.getStatusCode().value())
+        );
+    }
+
+    @ExceptionHandler(Exception.class) // global exception handling
+    public ResponseEntity<ExceptionResponse> handleException(Exception exception){
+        return buildResponseEntity(exception, HttpStatus.BAD_REQUEST);
+    }
+
+    private ResponseEntity<ExceptionResponse> buildResponseEntity(Exception exception, HttpStatus httpStatus){
+        ExceptionResponse error = new ExceptionResponse();
+        // set the params
+        error.setStatus(httpStatus.value());
+        error.setMessage(exception.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, httpStatus);
+    }
+}
